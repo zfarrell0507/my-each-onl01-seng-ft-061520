1c2d8ba313e93df683eb260f1a637111e3f990fa
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-26 15:47:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+    words = ['hi', 'hello', 'bye', 'goodbye']
+def my_each(words)
+  if block_given?
+    counter = 0 
+    while counter < words.length 
+      yield words[counter]
+      counter += 1
+    end 
+    words
+  end 
 end
+   my_each(['hi', 'hello', 'bye', 'goodbye']) do |word|
+    end
